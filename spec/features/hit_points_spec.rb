6fb20006e4a7a626878520aff21b00fb59b03078
Andrew Dowell <adowellphoto@gmail.com>
2015-10-28 16:37:40 +0000

    Chapter 28 completed: Multiplayer

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
 feature 'Hit points' do
-  scenario 'displays the opponents hit points' do
+  scenario 'displays both players hit points' do
     sigh_in_and_play
     expect(page).to have_content 'Incredible Yev: 100HP'
+    expect(page).to have_content 'Almighty Tom: 100HP'
   end
-  scenario 'reduces the opponents hit points after being attacked' do
+
+  scenario 'reduces both players hit points after being attacked' do
     sigh_in_and_play
     click_button('Attack')
     click_button('OK')
     expect(page).to have_content 'Incredible Yev: 90HP'
+    click_button('Attack')
+    click_button('OK')
+    expect(page).to have_content 'Almighty Tom: 90HP'
   end
 
 end
