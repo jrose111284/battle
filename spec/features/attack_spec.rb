2312d922c6bd391f8476911cceb4f3a14568ffdf
Robert Youngman <rob_youngman@yahoo.com>
2015-10-29 15:05:28 +0000

    Heal buttton spec

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,4 +6,10 @@ feature 'Attacking' do
     click_button('Attack')
     expect(page).to have_content 'Almighty Tom attacks Incredible Yev!'
   end
+
+  scenario 'heal' do
+    sign_in_and_play
+    click_button('Heal')
+    expect(page).to have_content 'Almighty Tom is healed'
+  end
 end
